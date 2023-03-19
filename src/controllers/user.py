1111304e6952673dc5df77969f3c67ca77a78ec4
Владимир Архипов <Skader-kot@mail.ru>
2023-03-19 07:06:26 +0300

    Bug fix

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -31,3 +31,8 @@ async def update_user(data: schemas.UserUpdate, services: ServiceFactory = Depen
 @router.put("/update_my_password", response_model=None, status_code=http_status.HTTP_200_OK)
 async def update_user_password(data: schemas.UserPasswordUpdate, services: ServiceFactory = Depends(get_services)):
     await services.user.user_password_update_by_user(data)
+
+
+@router.put("/update/avatar", response_model=None, status_code=http_status.HTTP_200_OK)
+async def update_avatar(file_id: uuid.UUID, service: ServiceFactory = Depends(get_services)):
+    pass
